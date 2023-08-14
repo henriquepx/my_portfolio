ba814ef4bce5498ff3107fe83c150088a64fc6ba
Henrique <henriquepinheiroxavier@gmail.com>
2023-08-14 19:36:04 -0300

    feat(style): redesign skills section

---
+++ src/components/LanguageSkill.jsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const LanguageContainer = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 15px;
+    background-color: #1f1f1f;
+    padding: .7rem;
+    border-radius: 10px;
+    cursor: pointer;
+    border: 1px solid #1f1f1f;
+
+    width: 370px;
+    &:hover {
+        background-color: #242323;
+        border: 1px solid #333232;
+    }
+    div {
+        h1 {
+            font-family: 'Montserrat', sans-serif;
+            color: #fffdfd;
+            margin-bottom: 5px;
+        }
+        p {
+            font-family: 'Montserrat', sans-serif;
+            color: #8a8a8a;
+            font-size: .8rem;
+        }
+    }
+`
+
+const LanguageSkill = ({ icon, title, description }) => {
+  return (
+    <LanguageContainer>
+        <img src={icon} alt="" />
+        <div>
+            <h1>{title}</h1>
+            <p>{description}</p>
+        </div>
+    </LanguageContainer>
+  )
+}
+
+LanguageSkill.propTypes = {
+    icon: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  };
+
+export default LanguageSkill;
--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import LanguageSkill from '../components/LanguageSkill';
 
 const SkillsContainer = styled.div`
     height: 100vh;
@@ -15,14 +16,26 @@ const SkillsSize = styled.div`
     width: 100%;
     max-width: 800px;
     margin: 0 auto;
+
     display: flex;
-    flex-wrap: wrap;
-    gap: 15px;
+    flex-direction: column;
 `
 const TitleSkills = styled.h1`
     font-family: 'Montserrat', sans-serif;
     font-size: 3rem;
     margin-bottom: 2rem;
+    color: white;
+    font-weight: 610;
+`
+const SkillsWrap = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+`
+const LearningSkills = styled.h2`
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.2rem;
+    margin: 4rem 0rem 1rem 0rem;
     color: white;
     font-weight: 610;
 `
@@ -32,8 +45,61 @@ const Skills = () => {
     <SkillsContainer id='skills'>
         <SkillsSize>
             <TitleSkills>Skills</TitleSkills>
-            <div>
-            </div>
+            <SkillsWrap>
+                <LanguageSkill
+                    icon="/skills/react.png" 
+                    title="React" 
+                    description="Biblioteca de interfaces interativas."
+                />
+                <LanguageSkill
+                    icon="/skills/javascript.png" 
+                    title="JavaScript" 
+                    description="Linguagem para desenvolvimento web."
+                />
+                <LanguageSkill
+                    icon="/skills/styledcomponents.png" 
+                    title="Styled Components" 
+                    description="Estilização de componentes."
+                />
+                <LanguageSkill
+                    icon="/skills/sass.png" 
+                    title="Sass" 
+                    description="Pré-processador de estilos."
+                />
+                <LanguageSkill
+                    icon="/skills/tailwind.png" 
+                    title="Tailwind" 
+                    description="Framework CSS utilitário."
+                />
+                <LanguageSkill
+                    icon="/skills/bootstrap.png" 
+                    title="Bootstrap" 
+                    description="Framework CSS responsivo."
+                />
+                <LanguageSkill
+                    icon="/skills/css.png" 
+                    title="CSS" 
+                    description="Estilização de páginas."
+                />
+                <LanguageSkill
+                    icon="/skills/html.png" 
+                    title="HTML" 
+                    description="Marcação de páginas web."
+                />
+            </SkillsWrap>
+            <LearningSkills>Learning:</LearningSkills>
+            <SkillsWrap>
+                <LanguageSkill
+                    icon="/skills/typescript.png" 
+                    title="TypeScript" 
+                    description="Superset do JavaScript."
+                />
+                <LanguageSkill
+                    icon="/skills/angular.png" 
+                    title="Angular" 
+                    description="Framework escalável."
+                />
+            </SkillsWrap>
         </SkillsSize>
     </SkillsContainer>
   )
