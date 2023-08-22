3333054abc888981658275b225c1ea3c506c3b75
Henrique <henriquepinheiroxavier@gmail.com>
2023-08-22 10:02:05 -0300

    fix: react icons

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import LanguageProject from './LanguageProject';
-import { FaLink, FaGithub } from 'react-icons/Fa';
+import { FaLink, FaGithub } from 'react-icons/fa';
 
 const ProjectContainerCard = styled.div`
     display: flex;
