fde1f9aad173954c12547e54fc4c9a14b2235bf3
Henrique <henriquepinheiroxavier@gmail.com>
2023-08-29 08:12:29 -0300

    styles: styles home page

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -9,16 +9,22 @@ const ContainerHome = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-
-    padding-left: 17rem;
+    @media (max-width: 1024px) {
+      height: 90vh;
+    }
     h1 {
       font-family: 'Montserrat', sans-serif;
       font-size: 5.5rem;
       font-weight: 800;
+
+      color: transparent;
       text-align: center;
+      line-height: 90px;
+      letter-spacing: -8px;
+      transform: skew(-10deg);
       transform: skewY(-2deg);
-      letter-spacing: 3px;
-      color: #504f4f;
+      text-shadow: #b6b5b5 -1px -3px, #3b3b3b -2px -4px, #363636 -3px -5px, #414040 -4px 4px;
+
       @media (max-width: 450px) {
         font-size: 2.5rem;
       }
@@ -28,9 +34,11 @@ const ContainerHome = styled.div`
         font-size: 2.5rem;
         font-weight: 600;
         font-style: italic;
+        transform: skew(-10deg);
         transform: skewY(-2deg);
         letter-spacing: 3px;
-        color: #504f4f;
+        text-shadow: #b6b5b5 -1px -3px, #3b3b3b -2px -4px, #363636 -3px -5px, #414040 -4px 4px;
+        color: transparent;
         @media (max-width: 450px) {
         font-size: 1.5rem;
         text-align: center;
