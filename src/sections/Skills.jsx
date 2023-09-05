97a2359b74d5493540d0ac2d3b251b92dcae477c
Henrique <henriquepinheiroxavier@gmail.com>
2023-09-05 08:15:46 -0300

    feat: translation skills section

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import LanguageSkill from '../components/LanguageSkill';
+import { useTranslation } from "react-i18next";
 
 const SkillsContainer = styled.div`
     height: 100%;
@@ -70,80 +71,84 @@ const TextAboutSkills = styled.div`
 `
 
 const Skills = () => {
+    const { t } = useTranslation();
+
+
   return (
     <SkillsContainer id='skills'>
         <SkillsSize>
-            <TitleSkills>Habilidades</TitleSkills>
+           
+<TitleSkills>{t("project.ProjectName")}</TitleSkills>
             <SkillsWrap>
                 <LanguageSkill
                     icon="/skills/react.png" 
                     title="React" 
-                    description="Biblioteca de interfaces interativas."
+                    description={t("project.ReactDescription")}
                 />
                 <LanguageSkill
                     icon="/skills/javascript.png" 
                     title="JavaScript" 
-                    description="Linguagem para desenvolvimento web."
+                    description={t("project.JavaScriptDescription")}
                 />
                 <LanguageSkill
                     icon="/skills/styledcomponents.png" 
                     title="Styled Components" 
-                    description="Estilização de componentes."
+                    description={t("project.StyledCompoDescription")}
                 />
                 <LanguageSkill
                     icon="/skills/sass.png" 
                     title="Sass" 
-                    description="Pré-processador de estilos."
+                    description={t("project.SassDescription")}
                 />
                 <LanguageSkill
                     icon="/skills/tailwind.png" 
                     title="Tailwind" 
-                    description="Framework CSS utilitário."
+                    description={t("project.TailwindDescription")}
                 />
                 <LanguageSkill
                     icon="/skills/bootstrap.png" 
                     title="Bootstrap" 
-                    description="Framework CSS responsivo."
+                    description={t("project.BoostrapDescription")}
                 />
                 <LanguageSkill
                     icon="/skills/css.png" 
                     title="CSS" 
-                    description="Estilização de páginas."
+                    description={t("project.CSSDescription")}
                 />
                 <LanguageSkill
                     icon="/skills/html.png" 
                     title="HTML" 
-                    description="Marcação de páginas web."
+                    description={t("project.HTMLDescription")}
                 />
             </SkillsWrap>
-            <LearningSkills>Aprendendo:</LearningSkills>
+            <LearningSkills>{t("project.ProjectLearning")}</LearningSkills>
             <SkillsWrap>
                 <LanguageSkill
                     icon="/skills/typescript.png" 
                     title="TypeScript" 
-                    description="Superset do JavaScript."
+                    description={t("project.TypeScriptDescription")}
                 />
                 <LanguageSkill
                     icon="/skills/angular.png" 
                     title="Angular" 
-                    description="Framework escalável."
+                    description={t("project.AngularDescription")}
                 />
                 <LanguageSkill
                     icon="/skills/sql.png" 
                     title="SQL" 
-                    description="Gerenciamento de Dados Relacionais."
+                    description={t("project.SQLDescription")}
                 />
                 <LanguageSkill
                     icon="/skills/nodejs.png" 
                     title="NodeJS" 
-                    description="Ambiente de Servidor JavaScript."
+                    description={t("project.NodeDescription")}
                 />
             </SkillsWrap>
             <TextAboutSkills>
-                <h3>Planos pro futuro.</h3>
-                <p>Atualmente, meu foco principal é no desenvolvimento front-end. Acredito que a experiência do usuário é fundamental, e estou empenhado em criar interfaces de usuário atraentes e intuitivas que proporcionem uma ótima experiência aos usuários finais.</p>
-                <p>Além disso, estou sempre em busca de aprendizado contínuo. Estou explorando novas tecnologias, como TypeScript e Angular, para expandir ainda mais meu conjunto de habilidades e abraçar desafios mais amplos no futuro.</p>
-                <p>Meu objetivo final é me tornar um desenvolvedor Fullstack, mas estou ciente da importância de construir uma base sólida no front-end antes de prosseguir para o back-end. Estou comprometido em me manter atualizado com as últimas tendências e melhores práticas do setor, para que eu possa contribuir de maneira significativa para projetos empolgantes e desafiadores.</p>
+                <h3>{t("project.SkillsTextT")}</h3>
+                <p>{t("project.SkillsText1")}</p>
+                <p>{t("project.SkillsText2")}</p>
+                <p>{t("project.SkillsText3")}</p>
             </TextAboutSkills>
         </SkillsSize>
     </SkillsContainer>
