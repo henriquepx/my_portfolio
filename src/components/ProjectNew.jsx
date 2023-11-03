68adf99aa36374067729c6b02846951a58e6a461
Henrique <henriquepinheiroxavier@gmail.com>
2023-11-03 10:28:28 -0300

    feat: add arrow to deploy and a link to repository

--- src/components/ProjectNew.jsx
+++ src/components/ProjectNew.jsx
@@ -19,29 +19,31 @@ const Meta = styled.div`
   height: 200px;
 `;
 
-const Details = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: -100%;
-  margin: auto;
-  transition: left 0.2s;
-  background: rgba(0, 0, 0, 0.2);
+const TitleProject = styled.a`
+  font-size: 1.7rem;
+  font-family: 'Montserrat', sans-serif;
   color: #fff;
-  padding: 10px;
-  width: 100%;
-  font-size: 0.9rem;
-`;
+  position: relative;
+  margin: 0;
+  display: block;
+  &:before {
+    content: "➲";
+    display: inline-block;
+    right: -15px;
+    opacity: 0;
+    position: absolute;
+    text-transform: none;
+    transition: all 0.5s cubic-bezier(0.215, 0.61, 0.355, 1) 0s;
+    visibility: hidden;
+    color: #fff;
+  }
+`
 
 const Description = styled.div`
   padding: 1.2rem 1.5rem;
   background: #1d1d1d;
   position: relative;
   z-index: 0;
-  a {
-    font-size: 1.7rem;
-    font-family: 'Montserrat', sans-serif;
-    color: #fff;
-  }
 
   h2 {
     font-size: 0.8rem;
@@ -78,10 +80,17 @@ const BlogCardContainer = styled.div`
   border: 2px solid #3d3d3d;
   overflow: hidden;
   z-index: 0;
+
   &:hover {
     ${Photo} {
       transform: scale(1.3) rotate(3deg);
       width: 100%;
+    }
+
+    ${TitleProject}:before {
+      right: 0;
+      opacity: 1;
+      visibility: visible;
     }
   }
 
@@ -103,6 +112,14 @@ const BlogCardContainer = styled.div`
   }
 `;
 
+const LinkToRepository = styled.a`
+  font-family: 'Montserrat', sans-serif;
+  color: #fff;
+  position: absolute;
+  bottom: 20px;
+`
+
+
 const ProjectNew = ({
   imgproject,
   titleproject,
@@ -115,14 +132,12 @@ const ProjectNew = ({
     <BlogCardContainer> 
       <Meta>
       <Photo imgproject={imgproject} />
-        <Details>
-          <a href={linkrepository} target='_blank' rel='noreferrer'>Repositório</a>
-        </Details>
       </Meta>
       <Description>
-        <a href={linkdeploy} target='_blank' rel='noreferrer'>{titleproject}</a>
+        <TitleProject href={linkdeploy} target='_blank' rel='noreferrer'>{titleproject}</TitleProject>
         <h2>{languagesproject}</h2>
         <p>{descproject}</p>
+        <LinkToRepository href={linkrepository} target='_blank' rel='noreferrer'>Repositório</LinkToRepository>
       </Description>
     </BlogCardContainer>
   );
